b6dac37c3f4bc83adeeacb74f6d71ff55e9bb00b
Jun <jun.w4ng@gmail.com>
2022-04-18 23:53:42 -0400

    Punto 1 agregado

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 import app from 'express'
 
-import Home from './home_routes'
-import Character from './character_routes'
+import Home from './home_routes.js'
+import Character from './character_routes.js'
 
 const routes = app.Router()
 
